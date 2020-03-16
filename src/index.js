929585d46757a83ad47fd54cac5d1f0df17db98f
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-03-16 11:40:30 +0200

    Add styles

--- src/index.js
+++ src/index.js
@@ -7,11 +7,40 @@ import ContactList from "./Components/ContactList/ContactList";
 
 const App = () => {
   return (
-    <Fragment>
-      <h1>Contact List</h1>
-      <div className="subtitle">APP</div>
-      <ContactList />
-    </Fragment>
+    <div className="container">
+      <div className="row">
+        <div class="panel panel-default">
+          <div class="panel-heading c-list">
+            <span class="title">Contacts</span>
+            <ul class="pull-right c-controls">
+              <li>
+                <a
+                  href="#cant-do-all-the-work-for-you"
+                  data-toggle="tooltip"
+                  data-placement="top"
+                  title="Add Contact"
+                >
+                  <i class="glyphicon glyphicon-plus"></i>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="hide-search"
+                  data-command="toggle-search"
+                  data-toggle="tooltip"
+                  data-placement="top"
+                  title="Toggle Search"
+                >
+                  <i class="fa fa-ellipsis-v"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <ContactList />
+      </div>
+    </div>
   );
 };
 
